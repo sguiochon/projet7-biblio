6b994993815202b7e0a82c05d853bc7b0216b705
Samuel Guiochon <samuel.guiochon@axa.fr>
2019-09-06 13:49:36 +0200

    Fix: url construction was incorrect

--- webclient-library/src/main/java/sam/biblio/web/webclient/CommonWebClient.java
+++ webclient-library/src/main/java/sam/biblio/web/webclient/CommonWebClient.java
@@ -4,16 +4,15 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.hateoas.hal.Jackson2HalModule;
-import org.springframework.http.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 import sam.biblio.dto.PageInfo;
-import org.apache.http.client.utils.URIBuilder;
 
-import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.util.Arrays;
+import java.util.Collections;
 
 
 public class CommonWebClient {
@@ -22,10 +21,9 @@ public class CommonWebClient {
     String resourcePath;
     String username;
     String password;
-    //URIBuilder uriBuilder;
-    StringBuilder stringBuilder;
+    private StringBuilder stringBuilder;
 
-    protected CommonWebClient(String apiEndpoint, String resourcePath, String username, String password) throws URISyntaxException {
+    protected CommonWebClient(String apiEndpoint, String resourcePath, String username, String password) {
         this.apiEndPoint = apiEndpoint;
         this.resourcePath = resourcePath;
         this.username = username;
@@ -35,76 +33,56 @@ public class CommonWebClient {
     /**
      * Builds a RestTemplate instance that fixes a bug in SpringBoot Web Client: http header Accept is not
      * valid for Spring Data REST API and must be forced to 'application/hal+json'.
-     * @return
+     *
      */
-    RestTemplate buildRestTemplate(){
+    RestTemplate buildRestTemplate() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         objectMapper.registerModule(new Jackson2HalModule());
 
         MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
 
-        messageConverter.setSupportedMediaTypes(MediaType.parseMediaTypes(Arrays.asList("*/*","application/hal+json;charset=utf-8","application/json;charset=utf-8")));
+        messageConverter.setSupportedMediaTypes(MediaType.parseMediaTypes(Arrays.asList("*/*", "application/hal+json;charset=utf-8", "application/json;charset=utf-8")));
         messageConverter.setObjectMapper(objectMapper);
 
-        return new RestTemplate(Arrays.asList(messageConverter));
+        return new RestTemplate(Collections.singletonList(messageConverter));
     }
 
-    HttpHeaders createHeaders(String username, String password){
+    HttpHeaders createHeaders(String username, String password) {
         return new HttpHeaders() {{
             String auth = username + ":" + password;
-            byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")) );
-            String authHeader = "Basic " + new String( encodedAuth );
-            set( "Authorization", authHeader );
+            byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+            String authHeader = "Basic " + new String(encodedAuth);
+            set("Authorization", authHeader);
         }};
     }
 
-    CommonWebClient addParam(PageInfo pageInfo){
-        if (pageInfo!=null){
-            //uriBuilder.addParameter("page", String.valueOf(pageInfo.getNumber()));
+    CommonWebClient addParam(PageInfo pageInfo) {
+        if (pageInfo != null) {
             this.addParam("page", String.valueOf(pageInfo.getNumber()));
-            //uriBuilder.addParameter("size", String.valueOf(pageInfo.getSize()));
             this.addParam("size", String.valueOf(pageInfo.getSize()));
         }
         return this;
     }
 
-    CommonWebClient addParam(String name, String value){
-        //uriBuilder.addParameter(name, value);
+    CommonWebClient addParam(String name, String value) {
         if (!stringBuilder.toString().contains("?"))
             stringBuilder.append("?");
+        if (!stringBuilder.toString().endsWith("?"))
+            stringBuilder.append("&");
         stringBuilder.append(name).append("=").append(value);
         return this;
     }
 
-    CommonWebClient setUrl(String url) throws URISyntaxException {
-        //uriBuilder = new URIBuilder(url);
+    CommonWebClient setUrl(String url) {
         stringBuilder = new StringBuilder(url);
         return this;
     }
 
-    String buildURL() throws URISyntaxException {
-        String urlString = stringBuilder.toString();//uriBuilder.build().toString();
-        //uriBuilder.clearParameters();
-        stringBuilder=null;
-        System.out.println(">>>>>>>>>>>>>>>>>> " + urlString);
+    String buildURL() {
+        String urlString = stringBuilder.toString();
+        stringBuilder = null;
         return urlString;
     }
-
-
-/*
-    String buildParams(String url, PageInfo pageInfo){
-        StringBuilder str = new StringBuilder(url);
-        if (pageInfo==null){
-            str.append("?page=0");
-        }
-        else{
-            str.append("?page=").append(pageInfo.getNumber());
-            str.append("&size=").append(pageInfo.getSize());
-        }
-        return str.toString();
-    }
-*/
-
 
 }
