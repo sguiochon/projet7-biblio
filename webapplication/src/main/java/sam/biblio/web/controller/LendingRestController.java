a67345fd6e5b6dc466d6e6275361924bd997e406
Samuel Guiochon <samuel.guiochon@axa.fr>
2019-10-17 11:15:12 +0200

    Fake modif

--- webapplication/src/main/java/sam/biblio/web/controller/LendingRestController.java
+++ webapplication/src/main/java/sam/biblio/web/controller/LendingRestController.java
@@ -1,18 +1,26 @@
 package sam.biblio.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import sam.biblio.web.dto.LendCopyDTO;
+import sam.biblio.web.service.LendCopyService;
 
 import java.security.Principal;
 
 @RestController
 public class LendingRestController {
 
+    @Autowired
+    LendCopyService lendCopyService;
+
     @PostMapping(value="/lend")
     public String lendCopy(Principal principal, @RequestBody LendCopyDTO lendCopyDTO){
         System.out.println(">>>>> demande de reservation d'un exemplaire: " + lendCopyDTO.toString());
+
+
+
         return "";
     }
 
--- webapplication/src/main/java/sam/biblio/web/service/LendCopyService.java
+++ webapplication/src/main/java/sam/biblio/web/service/LendCopyService.java
@@ -1,4 +1,26 @@
 package sam.biblio.web.service;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sam.biblio.model.library.Lending;
+import sam.biblio.web.webclient.DocumentWebClient;
+
+@Service
 public class LendCopyService {
+
+    private final static Log LOG = LogFactory.getLog(LendCopyService.class);
+
+    @Autowired
+    private DocumentWebClient documentWebClient;
+
+    //Todo: finaliser le service...
+
+    public Lending lendCopy(Long documentID, String userLogin){
+
+        return null;
+    }
+
 }
